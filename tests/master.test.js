9db95a299fb72ebcb5c6176aa9a1b10e2f2f29c6
Anatoly Ostroumov <ostroumov@teksi.ru>
2013-07-12 22:51:32 +0400

    unit test for master

---
+++ tests/master.test.js
@@ -0,0 +1,43 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    redis = require('redis'),
+    assemblage = require("../index");
+    
+vows.describe('Master module test')
+	.addBatch({
+	  'Redis':{
+		    'topic':redis.createClient(6379,'127.0.0.1'),
+			'accept connections, allows to add and delete key':function(topic){
+			  topic.on("error",function(err){
+			      throw err;  
+			  });	
+			  var keyName="string key"+Math.random();
+			  topic.set(keyName, "string val", function(err){
+				  if(err) throw err;
+				  topic.get("string key",function(err2,value){
+				    if(err) throw err;
+				    assert.equal(value,"string val",'Key was not set!');
+				    topic.del(keyName,topic.print);
+				  });  
+			  });
+			}	
+	    }
+	})
+	.addBatch({
+	   'Master':{
+		   'topic':assemblage.createMaster("mycluster",{
+			   client:redis.createClient(6379,'127.0.0.1')
+			}),
+			'do creates a job and fires a callback without error and with jobId':function(topic){
+				topic.addJob({_worker:"1234", key1:"fgdffg"}, 
+				function(err, jobId){
+					if(err){
+						throw err;
+					}
+					assert.isString(jobId,'Job id is not string!');
+					assert.isTrue(jobId.lenght()>3,'Job id is too short!');
+				});
+			}
+		}
+	})
+	.export(module);
